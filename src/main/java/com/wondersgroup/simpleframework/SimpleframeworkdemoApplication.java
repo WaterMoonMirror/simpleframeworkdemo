7b2e38b33a3414527d5768914261042a93bac859
lizhu <lzlj21@163.com>
2021-01-07 10:31:44 +0800

    Initial commit

---
+++ src/main/java/com/wondersgroup/simpleframework/SimpleframeworkdemoApplication.java
@@ -0,0 +1,13 @@
+package com.wondersgroup.simpleframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleframeworkdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleframeworkdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wondersgroup/simpleframework/SimpleframeworkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wondersgroup.simpleframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleframeworkdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
